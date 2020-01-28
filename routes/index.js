4610d001fca28240aef42c16b32573775e12e2af
Prateekkhosla1996 <53938174+Prateekkhosla1996@users.noreply.github.com>
2020-01-29 02:25:45 +0530

    new commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
+console.log('router get loaded');
+
 router.get('/',homeController.home)
 
-
-
+router.use('/users',require('./user'));
+// router.use('/routerName',require('./routerfile'))
 module.exports=router;
